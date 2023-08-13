502d7c68122914edbc0002a91f7f08d2acbf1ad5
gsspdev <dev@gssp.io>
2023-08-13 02:44:56 -0700

    made a filter struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 // rustalogue::lib
 
 pub mod oscillator;
-pub mod FreqFilter;
+pub mod freq_filter;
 pub mod envelope;
 pub mod lfo;
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,15 @@
-// rustalogue::main
-
+extern crate rustalogue;
 extern crate meval;
-
-use rustalogue::oscillator as osc;
-use rustalogue::FreqFilter;
-// use rustalogue::envelope as enve;
-// use rustalogue::lfo;
-
-use osc::shape;
-// use enve::settings as envSet;
-// use lfo::rate as lfoRate;
+use rustalogue::freq_filter::FreqFilter;
 
 pub fn main() {
-    shape::sin();
-    shape::saw();
-    FreqFilter::giveType();
+    let mut filter: FreqFilter = FreqFilter {
+        filter_type: None,
+        frequency: 520.0,
+        qfactor: 0.72 };
 
+    // let mut filter2 = <FreqFilter as Default>::default;
+    
+    filter.set_filter_type("lowpass");
+    // filter2.set_filter_type(filter: "highpass");
 }
