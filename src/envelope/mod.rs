5c45a51c555a6e5f2bb9ab4980103702840bab44
gsspdev <dev@gssp.io>
2023-08-14 12:34:42 -0700

    Update envelope module with new trait and implementation

--- src/envelope/mod.rs
+++ src/envelope/mod.rs
@@ -3,6 +3,21 @@ pub mod envelope {
        attack: f32,
        decay: f32,
        sustain: f32,
-       release: f32xw
+       release: f32
         }
+}
+
+trait TraitName {
+    fn new(attack: f32, decay: f32, sustain: f32, release: f32) -> Self;
+}
+
+impl TraitName for envelope::Envelope {
+    fn new(attack: f32, decay: f32, sustain: f32, release: f32) -> Self {
+        Self {
+            attack,
+            decay,
+            sustain,
+            release
+        }
+    }
 }
